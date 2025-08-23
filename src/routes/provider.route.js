157c80f11eb5251cd41b71808e5fd205f5a37d3f
Charbel Assaker <charbelassaker@live.com>
2025-08-23 16:29:55 +0300

    allow providers

--- src/routes/provider.route.js
+++ src/routes/provider.route.js
@@ -12,7 +12,7 @@ import permit from "../middlewares/permit.middleware.js";
 
 const router = Router();
 
-router.get("/", auth, permit("ADMIN", "PROVIDER"), all);
+router.get("/", all); // auth, permit("ADMIN", "PROVIDER"),
 router.get("/:id", auth, permit("ADMIN", "PROVIDER"), byId);
 router.post("/", auth, permit("ADMIN", "PROVIDER"), create);
 router.put("/:id", auth, permit("ADMIN", "PROVIDER"), update);
