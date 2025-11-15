1b76d6c36c50459ead1c90e6bb65a0f51d48f8c1
Charbel Assaker <charbel.assaker@coderaas.com>
2025-11-15 15:00:54 +0200

    fix: devices issue 2

--- src/services/device.service.js
+++ src/services/device.service.js
@@ -45,10 +45,36 @@ export async function findById(req, id) {
   return device;
 }
 export async function findByCode(req, code) {
-  return prisma.device.findFirst({
+  const device = prisma.device.findFirst({
     where: { code, providerId: req.user?.providerId || undefined },
     include: { provider: true },
   });
+
+  console.log("HEADERS: ", req.headers);
+
+  const timezone = req.headers.timezone || "UTC";
+
+  const now = new Date();
+
+  const dateFormatter = new Intl.DateTimeFormat("en-CA", {
+    timeZone: timezone,
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
+
+  const timeFormatter = new Intl.DateTimeFormat("en-GB", {
+    timeZone: timezone,
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  });
+
+  device.dateNow = dateFormatter.format(now); // "2025-02-15"
+  device.timeNow = timeFormatter.format(now); // "14:37:22"
+
+  return device;
 }
 export async function update(req, id, data) {
   if (req.user?.providerId) data.providerId = req.user?.providerId;
